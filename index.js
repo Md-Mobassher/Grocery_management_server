f9baa84b1d26c6bebae6620ebe58f162f1b7516e
Md-Mobassher <you@example.com>
2022-04-30 18:12:49 +0600

    groca grocery server created

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+// use middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send("Server is running");
+})
+
+app.listen(port, () => {
+    console.log('Lisetening to port', port)
+})
+
+
