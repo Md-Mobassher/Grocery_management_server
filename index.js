7708cc8c9ed6f9cdc3d1feaf92461a53d0fbc6bd
Md-Mobassher <you@example.com>
2022-05-01 18:37:26 +0600

    single product api added to server

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -29,6 +29,14 @@ async function run() {
             console.log(products);
             res.send(products);
         });
+
+        // get single product
+        app.get('/product/:id', async(req, res) =>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const product = await productCollection.findOne(query);
+            res.send(product);
+        })
 
     }
     finally{
