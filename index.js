3278f4843d568ac09f4b315b1fac6207bfb5a6d9
Md-Mobassher <you@example.com>
2022-05-07 22:32:47 +0600

    delet order api added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -9,6 +10,21 @@ const app = express();
 // use middleware
 app.use(cors());
 app.use(express.json());
+
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'Unauthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+        if (error) {
+            return res.status(403).send({ message: 'Forbidden access' });
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dl65k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -20,6 +36,17 @@ async function run() {
         await client.connect();
         const inventoryCollection = client.db('grocaGrocery').collection('inventory');
         const orderCollection = client.db('grocaGrocery').collection('order');
+
+        
+        // AUTH
+        app.post('/login', async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1d'
+            });
+            res.send({ accessToken });
+        })
+
 
         // get all inventory
         app.get('/inventory', async(req, res) => {
@@ -50,26 +77,59 @@ async function run() {
             const id = req.params.id;
             const query = {_id: ObjectId(id)};
             const result = await inventoryCollection.deleteOne(query);
-            if (result.deletedCount === 1) {
-                res.send("Successfully deleted one document.");
-              } else {
-                res.send("No documents matched. Deleted 0 documents.");
-              }           
+            res.send(result)         
         })
 
-        
+        // update inventory
+        app.put('/inventory/:id', async(req, res) =>{
+            const id = req.params.id;
+            const updateQuantity = req.body;
+            const filter = {_id: ObjectId(id)};
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    quantity: updateQuantity.quantity
+                }
+            };
+            const result = await inventoryCollection.updateOne(filter, updateDoc, options);
+            res.send(result);
+
+        })
+
 
 
         // Order collection api
 
-        app.get('/order', async(req,res) => {
-            const query = {};
-            const cursor = orderCollection.find(query);
-            const orders = await cursor.toArray();
-            res.send(orders);
+        // get order
+        app.get('/order', verifyJWT, async (req, res) => {
+            const decodedEmail = req.decoded.email;
+            const email = req.query.email;
+            if (email === decodedEmail) {
+                const query = { email: email };
+                const cursor = orderCollection.find(query);
+                const orders = await cursor.toArray();
+                res.send(orders);
+            }
+            else{
+                res.status(403).send({message: 'forbidden access'})
+            }
+        })
+       
+
+        // post order
+        app.post('/order', async (req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.send(result);
         })
 
-
+        // DELETE order
+        app.delete('/order/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await orderCollection.deleteOne(query);
+            res.send(result)         
+        })
         
     }
     finally{
